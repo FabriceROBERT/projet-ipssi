e1fbae7291172323e6aaa402eea28a574c669bd8
1804rbt <fabricerob@yahoo.fr>
2025-01-17 15:27:44 +0100

    Revert "initialization v2"
    This reverts commit 7ff112c60622e254f36a4158a87e4069c7d8baf2.

--- back-end/server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const db = require("./config/db");
-const cors = require("cors");
-
-const app = express();
-const PORT = 3002;
-app.use(cors());
-app.use(express.json());
--- front-end/src/components/Header.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Header({ children }) {
-  return <div className="bg-slate-950  z-10 w-full h-1/2">{children}</div>;
-}
